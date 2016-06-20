30c9e55c6e2a42ccbef7b8c132ab7f436b059066
MikeKouwenhoven <mkouwenhoven12@gmail.com>
2016-06-20 12:25:16 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/groep4/inf1e_prj_app/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.groep4.inf1e_prj_app;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
